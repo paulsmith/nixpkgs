859b7433c20dd2da4e96e2433941ae209ef78c67
Kirill A. Korinsky <kirill@korins.ky>
2022-06-27 21:59:21 +0200

    prl-tools: 17.1.4-51567

--- pkgs/os-specific/linux/prl-tools/default.nix
+++ pkgs/os-specific/linux/prl-tools/default.nix
@@ -14,7 +14,7 @@ let
       else throw "Parallels Tools for Linux only supports {aarch64,x86_64,i686}-linux targets";
 in
 stdenv.mkDerivation rec {
-  version = "${prl_major}.1.1-51537";
+  version = "${prl_major}.1.4-51567";
   prl_major = "17";
   pname = "prl-tools";
 
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   # => ${dmg}/Parallels\ Desktop.app/Contents/Resources/Tools/prl-tools-lin.iso
   src = fetchurl {
     url =  "https://download.parallels.com/desktop/v${prl_major}/${version}/ParallelsDesktop-${version}.dmg";
-    sha256 = "1ab5jwbg3jgvfwx8kwxwhhrsdp9wz4g9q7fg4z0hhd8v9pgy4yxx";
+    sha256 = "sha256-gjLxQOTFuVghv1Bj+zfbNW97q1IN2rurSnPQi13gzRA=";
   };
 
   hardeningDisable = [ "pic" "format" ];
